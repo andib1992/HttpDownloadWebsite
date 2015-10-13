cf001d3b49aaab50649cd799969c3da5daec6cb4
andi <andreas.bauer1992@gmail.com>
2015-10-13 12:27:08 +0200

    project created

---
+++ src/de/httpdownload/Main.java
@@ -0,0 +1,16 @@
+package de.httpdownload;
+
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+import com.google.api.client.http.GenericUrl;
+
+public class Main {
+	public static Scanner scanner;
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
